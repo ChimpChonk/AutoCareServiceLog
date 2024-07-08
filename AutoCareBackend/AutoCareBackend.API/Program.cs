a6a19a35fef6d9478f85c8b2dd5098dfa66a644b
ChimpChonk <sanjit.poudel28@gmail.com>
2024-07-08 13:20:48 +0200

    wip

--- AutoCareBackend/AutoCareBackend.API/Program.cs
+++ AutoCareBackend/AutoCareBackend.API/Program.cs
@@ -8,9 +8,8 @@ namespace AutoCareBackend.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            // builder.Services.AddSingleton
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
---
+++ AutoCareBackend/AutoCareBackend.Repo/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using System.IO;
+
+namespace AutoCareBackend.Repo.Data
+{
+    public class DataContext
+    {
+        
+
+    }
+    
+}
